bb95b63bfad4470408549ac0b0fbc7c8de196a0e
Kavindya Rajapaksha <it21815782@my.sliit.lk>
2023-09-25 14:06:34 +0530

    Creates the working environment.

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+// Import the Express module
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
